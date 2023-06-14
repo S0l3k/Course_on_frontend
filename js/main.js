3e38f3e3dc47896f63ae942b3dd78d3553151c56
S0l3k <turtleteamwcg@gmail.com>
2023-06-14 17:56:47 +0300

    fix: модальное окно, однако оно не открывается

--- js/main.js
+++ js/main.js
@@ -1,23 +1,41 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    prevEl: '.hotel-slider__button--prev',
-    nextEl: '.hotel-slider__button--next',
-  },
-  
-});
+    // Navigation arrows
+    navigation: {
+      prevEl: '.hotel-slider__button--prev',
+      nextEl: '.hotel-slider__button--next',
+    },
+  });
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    prevEl: '.reviews-slider__button--prev',
-    nextEl: '.reviews-slider__button--next',
-  },
-  
+    // Navigation arrows
+    navigation: {
+      prevEl: '.reviews-slider__button--prev',
+      nextEl: '.reviews-slider__button--next',
+    },
+  });
+
+  var modalButton = $("[date-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 });
